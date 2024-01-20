45b7e7c8e8158c5d2dc9bc884f9535ee3d0d917e
Zach <147861361+1054009@users.noreply.github.com>
2024-01-19 19:15:18 -0500

    Minor code cleanups

--- gm_dme/src/gm_dme.cpp
+++ gm_dme/src/gm_dme.cpp
@@ -1,11 +1,11 @@
-#include <GarrysMod/Lua/Interface.h>
 #include <GarrysMod/FactoryLoader.hpp>
 #include <GarrysMod/InterfacePointers.hpp>
-#include <MinHook.h>
-#include <iclientrenderable.h>
+#include <GarrysMod/Lua/Interface.h>
 #include <GarrysMod/Lua/LuaShared.h>
+#include <iclientrenderable.h>
+#include <MinHook.h>
 
-#define LUASHAREDBULLSHIT "LUASHARED003"
+#define LUA_SHARED_INTERFACE_VERSION "LUASHARED003"
 #define VENGINE_HUDMODEL_INTERFACE_VERSION "VEngineModel016"
 
 template<typename Fn>
@@ -13,6 +13,8 @@ inline Fn getvfunc(const void* v, int i)
 {
 	return (Fn) * (*(const void***)v + i);
 }
+
+class IVModelRender;
 
 enum
 {
@@ -22,18 +24,14 @@ enum
 };
 
 GarrysMod::Lua::ILuaShared* pLuaShared;
+GarrysMod::Lua::ILuaBase* LUA_CL;
 int _G_hook_Call = NULL;
 
-typedef void(__thiscall* fnDrawModelExecute)(void*, DrawModelState_t&, ModelRenderInfo_t&, matrix3x4_t*);
+typedef void(__thiscall* fnDrawModelExecute)(IVModelRender*, DrawModelState_t&, ModelRenderInfo_t&, matrix3x4_t*);
 fnDrawModelExecute ofnDrawModelExecute;
 
-void __fastcall hkDrawModelExecute(void* pthis, DrawModelState_t& state, ModelRenderInfo_t& pInfo, matrix3x4_t* pCustomBoneToWorld = NULL)
+void __fastcall hkDrawModelExecute(IVModelRender* _this, DrawModelState_t& state, ModelRenderInfo_t& pInfo, matrix3x4_t* pCustomBoneToWorld = NULL)
 {
-	// Death and despair
-	GarrysMod::Lua::ILuaBase* LUA_CL = (GarrysMod::Lua::ILuaBase*)pLuaShared->GetLuaInterface(LUA_CLIENT);
-	if (!LUA_CL)
-		return ofnDrawModelExecute(pthis, state, pInfo, pCustomBoneToWorld);
-	
 	LUA_CL->ReferencePush(_G_hook_Call);
 	{
 		LUA_CL->PushString("PreDrawModelExecute");
@@ -42,7 +40,7 @@ void __fastcall hkDrawModelExecute(void* pthis, DrawModelState_t& state, ModelRe
 	}
 	LUA_CL->Call(3, 0);
 
-	ofnDrawModelExecute(pthis, state, pInfo, pCustomBoneToWorld);
+	ofnDrawModelExecute(_this, state, pInfo, pCustomBoneToWorld);
 
 	LUA_CL->ReferencePush(_G_hook_Call);
 	{
@@ -82,10 +80,19 @@ GMOD_MODULE_OPEN()
 
 	// Get Lua shared so we can use the hook.Call we just got
 	SourceSDK::FactoryLoader factory_LuaShared("lua_shared");
-	pLuaShared = factory_LuaShared.GetInterface<GarrysMod::Lua::ILuaShared>(LUASHAREDBULLSHIT);
+	pLuaShared = factory_LuaShared.GetInterface<GarrysMod::Lua::ILuaShared>(LUA_SHARED_INTERFACE_VERSION);
 	if (!pLuaShared)
 	{
-		LUA->ThrowError("NO LUA SHARED RETARD");
+		LUA->ThrowError("Can't find Lua Shared");
+		return 0;
+	}
+
+	// Get the clientside Lua state
+	LUA_CL = (GarrysMod::Lua::ILuaBase*)pLuaShared->GetLuaInterface(LUA_CLIENT);
+	if (!LUA_CL)
+	{
+		// What are we running as??
+		LUA->ThrowError("No ClientSide Lua state");
 		return 0;
 	}
 
@@ -94,7 +101,7 @@ GMOD_MODULE_OPEN()
 	IVEngineClient* iModelRender = factory_Engine.GetInterface<IVEngineClient>(VENGINE_HUDMODEL_INTERFACE_VERSION);
 	if (!iModelRender)
 	{
-		LUA->ThrowError("NO MODEL RENDER RETARD");
+		LUA->ThrowError("Can't find IVModelRender");
 		return 0;
 	}
 
